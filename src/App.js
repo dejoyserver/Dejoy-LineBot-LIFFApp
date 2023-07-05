d61393f29dc66ae0f327146508ed88e13d013192
thomas0913 <408440021@gms.tku.edu.tw>
2023-07-05 11:19:33 +0800

    testing Qrcode Printer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
-import './components/LineAccountProfiles';
 import React from 'react';
-import LineAccountProfiles from './components/LineAccountProfiles';
+import { QRCodeSVG } from 'qrcode.react';
 
 function App() {
+  const [profiles, setProfiles] = useState({
+    userId: "",
+    displayName: "",
+    pictureUrl: "",
+    statusMessage: ""
+  });
+
+  useEffect(() => {
+    liff.init({
+        liffId: '2000018050-d5JXzaMR',
+        withLoginOnExternalBrowser: true,
+    }).then(() => {
+        liff.getProfile().then((profile) => {
+            setProfiles({
+                userId: profile.userId,
+                displayName: profile.displayName,
+                pictureUrl: profile.pictureUrl,
+                statusMessage: profile.statusMessage
+            });
+        });
+    }).catch((err) => {
+        console.log(err);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>This app is for linebot using liff webpage.</h1>
-      <LineAccountProfiles/>
+      <p>userId: {profiles.userId}</p>
+      <p>displayName: {profiles.displayName}</p>
+      <p>pictureUrl: {profiles.pictureUrl}</p>
+      <p>statusMessage: {profiles.statusMessage}</p>
+      <hr/>
+      <QRCodeSVG value={`https://3f72-163-13-133-72.ngrok-free.app/dejoy/linebot/getMember?userId=${profiles.userId}`}/>
     </div>
   );
 }
---
+++ src/components/QrcodePrinter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import liff from "@line/liff";
+
+const QrcodePrinter = () => {
+    
+
+    return(
+        <div>
+            <p>https://3f72-163-13-133-72.ngrok-free.app/dejoy/linebot/getMember?userId={}</p>
+        </div>
+    );
+}
+
+export default QrcodePrinter;
